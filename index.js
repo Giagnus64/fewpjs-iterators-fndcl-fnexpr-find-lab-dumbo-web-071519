e0dbb3f0b47b9e138a6d0f8a13ec42d1e39c0ddd
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-09-02 11:37:51 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" }
+  //...
+]
+
+function superbowlWin(objArr){
+const yearDesc =  objArr.find(function(obj){
+  return obj.result === "W";
+})
+if (yearDesc){
+  return yearDesc.year
+}
+return undefined
+}
+superbowlWin(record);
